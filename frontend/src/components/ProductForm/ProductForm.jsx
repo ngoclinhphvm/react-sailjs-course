9259e611a3838f8f06471a3a842a309edf542ec3
Ngoc Linh Pham <121231072+ngoclinhphvm@users.noreply.github.com>
2025-06-26 23:39:02 +0700

    feat: add ProductForm component with styling and functionality for adding new products

---
+++ frontend/src/components/ProductForm/ProductForm.jsx
@@ -0,0 +1,41 @@
+import { useState } from "react";
+import "./ProductForm.css";
+
+export default function ProductForm({ onAddProduct }) {
+  const [formData, setFormData] = useState({ name: "", price: "" });
+
+  function handleChange(event) {
+    const { name, value } = event.target;
+    setFormData((prevFormData) => ({ ...prevFormData, [name]: value }));
+  }
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    onAddProduct(formData);
+    setFormData({ name: "", price: "" });
+  }
+
+  return (
+    <div className="form-container">
+      <h2>Add New Product</h2>
+      <form className="product-form" onSubmit={handleSubmit}>
+        <p>
+          <label htmlFor="name">Product Name: </label>
+          <input type="text" name="name" id="name" onChange={handleChange} />
+        </p>
+        <p>
+          <label for="price">Price: </label>
+          <input
+            type="number"
+            name="price"
+            id="price"
+            onChange={handleChange}
+          />
+        </p>
+        <p className="button">
+          <button type="submit">Add Product</button>
+        </p>
+      </form>
+    </div>
+  );
+}
