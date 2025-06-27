6357991c8810a34271206cb9bfde544ca2744fd9
Ngoc Linh Pham <121231072+ngoclinhphvm@users.noreply.github.com>
2025-06-27 12:23:18 +0700

    refactor: update Header component by removing unnecessary elements and integrating SearchBar

--- frontend/src/components/Header/Header.jsx
+++ frontend/src/components/Header/Header.jsx
@@ -1,25 +1,33 @@
 import "./Header.css";
 
+function SearchBar() {
+  return (
+    <div className="search-bar">
+      <button className="search-button">
+        <img src="https://img.icons8.com/?size=100&id=14079&format=png&color=000000" />
+      </button>
+      <input type="text" placeholder="Search anything..." />
+    </div>
+  );
+}
+
 const Header = () => {
   return (
     <header className="header">
-      <div className="header-container">
-        <h1 className="header-title">My CMS</h1>
-        <div className="header-link-container">
-          <a href="/" className="header-link">
-            Dashboard
-          </a>
-          <a href="/" className="header-link">
-            Products
-          </a>
-          <a href="/" className="header-link">
-            Categories
-          </a>
-          <a href="/" className="header-link">
-            Settings
-          </a>
-        </div>
-        <img src="/icons/profile.png" />
+      <h1 className="header-title">My CMS</h1>
+      <div className="header-actions">
+        <SearchBar />
+        <button className="create-button">Create</button>
+
+        <button className="chip">
+          <img src="https://img.icons8.com/?size=100&id=16008&format=png&color=000000" />
+        </button>
+        <button className="chip">
+          <img src="https://img.icons8.com/?size=100&id=461&format=png&color=000000" />
+        </button>
+        <button className="chip profile">
+          <img src="https://images.genius.com/196ba084f993ae312b3f77be14bfd964.1000x1000x1.png" />
+        </button>
       </div>
     </header>
   );
